e85ac3cb7494e06193fc29c6dea178ce64f552e2
Alex <conelalex89@gmail.com>
2022-01-04 02:26:58 -0500

    Se crea el nav y se genera la ruta para los productos

---
+++ src/components/Products/index.js
@@ -0,0 +1,10 @@
+//Este es el componente de entrada para todos los productos (Administrador de productos)
+import React from "react";
+
+//esta es otra forma para exportar un compoenente desde una sola linea
+export default function Products() {
+    return (
+        <div>Productos</div>
+    );
+} 
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Router from './Router'
+
+import NavBar from './components/app/NavBar.jsx'
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <NavBar />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
